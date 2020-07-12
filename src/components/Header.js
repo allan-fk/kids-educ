fb4c39207e6d1e477a468a253276ba1c96ad5947
klein <allan.klein@epitech.eu>
2020-07-12 20:56:24 +0200

    header search-bar and home message

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Layout } from 'antd';
+import { Layout, Input, Button, Row, Col } from 'antd';
+import { SearchOutlined, PlusCircleFilled, CalendarOutlined } from '@ant-design/icons';
 import styled from 'styled-components'
 import * as palette from '../Variables'
 
@@ -23,7 +24,76 @@ const SideBarContainer = styled.div`
 export default class SideBarLeft extends Component {
   render() {
     return (
-      <Header style={{ backgroundColor: 'red', paddingLeft: '270px' }}>Header</Header>
+      <Header style={{
+        backgroundColor: '#f0f4fb',
+        paddingLeft: '270px',
+        height: '400px',
+      }}>
+        <Row>
+          <Col>
+            <Input
+              size="large"
+              placeholder="Rechercher"
+              prefix={<SearchOutlined />}
+              style={{ width: '300px', margin: '0 10px' }}
+            />
+            <Button type="primary">
+              <PlusCircleFilled />
+          Activite
+        </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col
+            style={{
+              backgroundColor: 'white',
+              width: '100vh',
+              borderRadius: '20px',
+              overflow: 'hidden',
+            }}
+          >
+            <Row
+              style={{
+                height: '140px'
+              }}
+            >
+              <Col
+                style={{
+                  padding: '20px 0px 0px 50px',
+                  // width: 'fit-content'
+                }}
+                span={12}
+              >
+                <h4
+                  style={{
+                    color: '#04c795',
+                    lineHeight: 'normal'
+                  }}
+                >
+                  Bienvenue sur votre dashboard
+                  <br />
+                  d'activites quotidiennes
+              </h4>
+                <p> <CalendarOutlined /> jeudi 3 Mars 2020</p>
+              </Col>
+              <Col span={12}>
+                <span
+                  style={{
+                    height: '400px',
+                    width: '400px',
+                    backgroundColor: 'rgb(39, 23, 164)',
+                    borderRadius: '50%',
+                    display: 'inline-block',
+                    right: '-100px',
+                    position: 'absolute',
+                    top: '-130px',
+                  }}
+                />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Header>
     )
   }
 }
